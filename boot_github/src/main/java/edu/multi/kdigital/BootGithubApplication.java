122c7c07d6842bfa69cad624f29dba82a5796f7d
joowon <joo1yeon@naver.com>
2022-03-21 15:45:17 +0900

    sts second commit

--- boot_github/src/main/java/edu/multi/kdigital/BootGithubApplication.java
+++ boot_github/src/main/java/edu/multi/kdigital/BootGithubApplication.java
@@ -10,6 +10,7 @@ public class BootGithubApplication {
 		SpringApplication.run(BootGithubApplication.class, args);
 		System.out.println("github push");
 		System.out.println("원격 저장소 추가 라인");
+		System.out.println("소스 주인 추가 라인");
 	}
 
 }
