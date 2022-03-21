be1afe39fdaae944882c7ab33e26b573a9839344
joo1yeon <50977497+joo1yeon@users.noreply.github.com>
2022-03-21 15:24:44 +0900

    Update BootGithubApplication.java

--- boot_github/src/main/java/edu/multi/kdigital/BootGithubApplication.java
+++ boot_github/src/main/java/edu/multi/kdigital/BootGithubApplication.java
@@ -9,6 +9,7 @@ public class BootGithubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootGithubApplication.class, args);
 		System.out.println("github push");
+		System.out.println("원격 저장소 추가 라인");
 	}
 
 }
